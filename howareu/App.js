327fe071ddc90b827035109545bafefd967ed94b
marduk beards <mike@luxvera.com>
2023-11-05 01:23:23 -0500

    looking good!!!!!

--- howareu/App.js
+++ howareu/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import { EntryScreen } from './screens';
 import { OverviewScreen } from './screens';
@@ -26,8 +28,17 @@ export default function App() {
   return (
     <NavigationContainer theme={MyTheme}>
       <Tab.Navigator>
-        <Tab.Screen name="Entry" component={EntryScreen} />
-        <Tab.Screen name="Overview" component={OverviewScreen} />
+        <Tab.Screen name="Entry" component={EntryScreen} options={{
+          headerTitle: "Create an entry",
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="note-plus" color={color} size={size} />
+          )
+        }} />
+        <Tab.Screen name="Calendar View" component={OverviewScreen} options={{
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="calendar" color={color} size={size} />
+          )
+        }} />
       </Tab.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
